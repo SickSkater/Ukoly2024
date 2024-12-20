77c80aebd60e5fc676b7f63b4416401a13f719b3
SickSkater <94305299+SickSkater@users.noreply.github.com>
2024-12-20 09:36:08 +0100

    Update Ctenar.py

--- Ukol_9/python/SystemKnihovny/Ctenar.py
+++ Ukol_9/python/SystemKnihovny/Ctenar.py
@@ -8,10 +8,28 @@ class Ctenar:
         self.cislo_prukazky = self.vygeneruj_cislo_prukazky()
 
     # doplňte potřebné gettry a settry
+    @property # dekorátor pro parametry objektu, bez manuálního volání
+    def jmeno(self) -> str:
+        return self._jmeno
+
+    @property
+    def prijmeni(self) -> str:
+        return self._prijmeni
+
+    @property
+    def cislo_prukazky(self) -> int:
+        return self._cislo_prukazky
+    
+    @cislo_prukazky.setter
+    def cislo_prukazky(self, cislo: int):
+        if cislo >= 1:
+            self._cislo_prukazky = cislo
+        else:
+            raise ValueError("ID of reader's card can not be negative! ")
 
     @staticmethod
     def vygeneruj_cislo_prukazky() -> int:
-        pass
+        return random.randint(10000, 99999) # melo by obsahovat ošetření pro duplikátní hodnoty, ale nechám to na náhodě, šance je malá
 
     def __str__(self) -> str:
-        return ""
+        return f"{self.jmeno} {self.prijmeni} (Průkazka: {self.cislo_prukazky})"
